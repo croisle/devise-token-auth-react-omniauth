7c6dece056fdbc598f1ead48610049ffa1d2aeb8
croisle <croisle@gmail.com>
2020-04-16 00:15:35 +0200

    add extends models to the user model plus uncommented the routes files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
         include DeviseTokenAuth::Concerns::SetUserByToken
+
+        protect_from_forgery unless: -> { request.format.json? }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-#   namespace :api, defautls: {format: :json} do
-#     namespace  :v1 do
-#        mount_devise_token_auth_for 'User', at: 'auth',
-#        controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
-#    end
-#  end
+  namespace :api, defautls: {format: :json} do
+    namespace  :v1 do
+       mount_devise_token_auth_for 'User', at: 'auth',
+       controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
+   end
+ end
 
  root 'pages#home'
  get '*all', to: 'pages#home', constraints: lambda { |req|
